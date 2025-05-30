ba298b933bfaa3859bd624c3be59597fc91122e2
luikalu <kalus.luis@gmail.com>
2025-05-30 13:03:52 +0200

    Finished Todo User.java, finished UserDto.java TODO. Added dependency to facilitate timestamp creation

--- api/src/main/java/de/unibayreuth/se/campuscoffee/api/dtos/UserDto.java
+++ api/src/main/java/de/unibayreuth/se/campuscoffee/api/dtos/UserDto.java
@@ -1,7 +1,10 @@
 package de.unibayreuth.se.campuscoffee.api.dtos;
 
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * DTO for user metadata.
@@ -10,6 +13,21 @@ import lombok.Data;
 @Data
 @Builder(toBuilder = true)
 public class UserDto {
-    private Long id; // id is null when creating a new task
-    // TODO: Implement user DTO.
+    private Long id;
+
+    @Pattern(regexp = "\\w+")
+    @NotBlank
+    private String loginName;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @Size(min = 1, max = 255)
+    @NotBlank
+    private String firstName;
+
+    @Size(min = 1, max = 255)
+    @NotBlank
+    private String lastName;
 }
--- domain/src/main/java/de/unibayreuth/se/campuscoffee/domain/model/User.java
+++ domain/src/main/java/de/unibayreuth/se/campuscoffee/domain/model/User.java
@@ -5,14 +5,58 @@ import lombok.Data;
 import java.io.Serial;
 import java.io.Serializable;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.*;
+
 /**
  * Domain class that stores the user metadata.
  */
+
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User implements Serializable {
+
     @Serial
     private static final long serialVersionUID = 1L;
 
-    private Long id; // null when the user has not been created yet
-    // TODO: Implement user domain class. Hint: the @Nullable annotations are important for the Lombok @Data annotation (see https://projectlombok.org/features/Data).
+    // ID can be null before persistence
+    @Nullable
+    private Long id;
+
+    // Must be set by JPA on creation
+    @NotNull
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    // Must be set by JPA on creation or update
+    @NotNull
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @NotBlank
+    @Pattern(regexp = "\\w+", message = "Login name must contain only word characters.")
+    private String loginName;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 1, max = 255)
+    private String firstName;
+
+    @NotBlank
+    @Size(min = 1, max = 255)
+    private String lastName;
 }
